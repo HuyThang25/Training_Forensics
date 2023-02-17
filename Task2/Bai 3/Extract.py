7006ed2fa6e3e8bdb40f0fff3fa9df416999756f
HuyThang25 <93728466+HuyThang25@users.noreply.github.com>
2023-02-17 11:48:46 +0700

    Update Extract.py

--- Task2/Bai 3/Extract.py
+++ Task2/Bai 3/Extract.py
@@ -5,16 +5,22 @@ def get_hidden_bit(value,vt):
 img = Image.open('New_Image.png')
 pixels = img.load()
 
-key=''
+key=[]
 width = img.size[0]
 height = img.size[1]
 for i in range(width):
   for j in range(height):
-    r,g,b = pixels[i,j]
-    key = key+str(get_hidden_bit(r,1))+str(get_hidden_bit(g,0))+str(get_hidden_bit(b,2))
+    r,g,b = pixels[j,i]
+    key.append(str(get_hidden_bit(r,1)))
+    key.append(str(get_hidden_bit(g,0)))
+    key.append(str(get_hidden_bit(b,2)))
 
+key = ''.join(key)
 key = [chr(int(key[i:i+8], 2)) for i in range(0,len(key),8)]
 key = ''.join(key)
-f = open('output.txt','w')
+
+f = open('output.txt','w',encoding='utf-8')
+key.encode('utf-8')
 f.write(key)
 f.close()
+
